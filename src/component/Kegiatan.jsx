1a333e5e1f9a02d737606eaf658cdac55f1a0e60
wandy <wandywahidin53@gmail.com>
2022-07-07 23:55:27 +0700

    add lazy

--- src/component/Kegiatan.jsx
+++ src/component/Kegiatan.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Lazy } from "react-lazy";
 
 const Kegiatan = ({ data }) => {
   return (
@@ -9,11 +10,13 @@ const Kegiatan = ({ data }) => {
             className="w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2"
             key={item.id}
           >
-            <img
-              className="w-full h-auto block"
-              src={item.images}
-              alt="kegiatan 1"
-            />
+            <Lazy>
+              <img
+                className="w-full h-auto block"
+                src={item.images}
+                alt="kegiatan 1"
+              />
+            </Lazy>
             <div className="absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 hover:duration-300 text-white">
               <p className="white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center">
                 {item.title}
--- src/pages/Acara.jsx
+++ src/pages/Acara.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import bg from "../assets/bg.jpg";
+import { Lazy } from "react-lazy";
 
 const Acara = ({ data }) => {
   return (
@@ -23,11 +24,13 @@ const Acara = ({ data }) => {
                   key={item.id}
                 >
                   <div className="bg-cover bg-bottom border w-full md:w-1/3 h-64 md:h-auto relative">
-                    <img
-                      className="w-full h-full object-cover"
-                      src={item.images}
-                      alt="santunan"
-                    />
+                    <Lazy>
+                      <img
+                        className="w-full h-full object-cover"
+                        src={item.images}
+                        alt="santunan"
+                      />
+                    </Lazy>
                     <div className="absolute text-xl">
                       <i className="fa fa-heart text-white hover:text-red-light ml-4 mt-4 cursor-pointer"></i>
                     </div>
@@ -49,7 +52,13 @@ const Acara = ({ data }) => {
                         </div>
                         <div className="w-full lg:w-5/5 mt-6 lg:mt-5 lg:px-4 text-center">
                           <button className="border font-semibold text-white bg-blue-700 rounded shadow-sm shadow-blue-700/50 border-gray-300 px-5 py-2 hover:bg-blue-500">
-                            <a href="https://drive.google.com/file/d/1zgpuhk-gcBhzOO3B_hrWRVzRvoqR3rlU/view?usp=sharing" rel="noopener noreferrer" target="_blank">Laporan Kegiatan</a>
+                            <a
+                              href="https://drive.google.com/file/d/1zgpuhk-gcBhzOO3B_hrWRVzRvoqR3rlU/view?usp=sharing"
+                              rel="noopener noreferrer"
+                              target="_blank"
+                            >
+                              Laporan Kegiatan
+                            </a>
                           </button>
                         </div>
                       </div>
